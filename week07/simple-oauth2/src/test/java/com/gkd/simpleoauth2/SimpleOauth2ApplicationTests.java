1413da618db68823130b4441e97032237942a2f0
guokaide <kaideguo@gmail.com>
2021-04-21 22:53:57 +0800

    Week07 homework

---
+++ week07/simple-oauth2/src/test/java/com/gkd/simpleoauth2/SimpleOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkd.simpleoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
